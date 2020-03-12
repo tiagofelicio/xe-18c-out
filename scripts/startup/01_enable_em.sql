5615f9fa474ef991630fd3cab0f1c288f487838d
Tiago Felício <62029871+tiagofelicio@users.noreply.github.com>
2020-03-12 20:58:46 +0000

    Update 01_enable_em.sql

--- scripts/startup/01_enable_em.sql
+++ scripts/startup/01_enable_em.sql
@@ -1,3 +1,10 @@
+-- ----------------------------------------------------------------------------------------------------------------------------
+-- File Name     : 01_enable_em.sql
+-- Author        : tiago felicio
+-- Description   :
+-- Call Syntax   : 01_enable_em.sql
+-- Last Modified : 2020/03/12
+-- ----------------------------------------------------------------------------------------------------------------------------
 
 exec dbms_xdb_config.setlistenerlocalaccess(false);
 exec dbms_xdb_config.setglobalportenabled(true);

8ce9b9649f76e72da2895ac24011c4cfaa3b636c
Tiago Felício <62029871+tiagofelicio@users.noreply.github.com>
2020-03-21 19:06:32 +0000

    Update 02_out_samples_install.sql

--- scripts/setup/02_out_samples_install.sql
+++ scripts/setup/02_out_samples_install.sql
@@ -8,13 +8,6 @@
 
 alter session set container = xepdb1;
 
-create tablespace stage
-datafile '/opt/oracle/oradata/XE/XEPDB1/stage01.dbf' size 1g
-autoextend on next 1g maxsize 10g
-nologging online permanent
-extent management local autoallocate default
-compress segment space management auto;
-
 create tablespace data
 datafile '/opt/oracle/oradata/XE/XEPDB1/data01.dbf' size 1g
 autoextend on next 1g maxsize 10g
@@ -22,4 +15,4 @@ nologging online permanent
 extent management local autoallocate default
 compress segment space management auto;
 
-@/opt/oracle/scripts/setup/out/samples/install.sql /opt/oracle/scripts/setup/out oracle metadata stage data temp
+@/opt/oracle/scripts/setup/out/samples/install.sql /opt/oracle/scripts/setup/out oracle metadata data temp
